459397121cce06e749061055ff834c66739c5ad9
unknown <cvoginski@DTPP027022.BRQ.COM>
2017-02-23 11:36:51 -0300

    gambs2

--- db.go
+++ db.go
@@ -61,7 +61,7 @@ func (dao *Dao) GetLastsComments(quantity int, up, down, left, right *geo.Point)
 
 	if err != nil {
 		fmt.Println(err)
-		dao.db.Prepare("alter table comment add column nick VARCHAR(30) default 'anonymous';")
+		stmt, err := dao.db.Prepare("alter table comment add column nick VARCHAR(30) default 'anonymous';")
 		_, err = stmt.Exec()
 		fmt.Println(err)
 		return nil
